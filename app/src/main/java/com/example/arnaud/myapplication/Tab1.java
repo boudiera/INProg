727660d9101a83493907767705c0703b2b7c34e4
boudiera <arnaud.boudier@ensimag.grenoble-inp.fr>
2016-03-26 09:25:11 +0100

    Init du commit

---
+++ app/src/main/java/com/example/arnaud/myapplication/Tab1.java
@@ -0,0 +1,17 @@
+
+package com.example.arnaud.myapplication;
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * Created by Arnaud on 22/03/2016.
+ */
+
+
+public class Tab1 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.onglet_evenement);
+    }
+}
